535ad2469c79dd222860600f33ba611f3db2ca4e
tfilippova <tphilippova@gmail.com>
2020-03-02 14:47:03 -0600

    Remove unused commented out code, add conditional statement to removeTodo method

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,3 @@
-// Using of Vue.js
-
 const todoApp = new Vue({
 	el: '#todo-app',
 	data: {
@@ -67,6 +65,9 @@ const todoApp = new Vue({
 		},
 		removeTodo(id) {
 			this.todoList = this.todoList.filter(todo => todo.id !== id);
+			if (this.todoList.length === 0) {
+				this.showFilter = false;
+			}
 		},
 
 
@@ -136,152 +137,3 @@ const todoApp = new Vue({
 
 	}
 })
-
-
-
-
-
-
-
-// Using of jQuery
-
-// $(document).ready(() => {
-//
-// 		$('.day-button').on('click', event => {
-// 			// $('.nav-menu').toggleClass('hide');
-// 			$(event.currentTarget).toggleClass('active');
-// 			$('#day-dropdown').slideToggle('slow');
-//
-// 	});
-//
-// 	$('.month-button').on('click', () => {
-// 		// $('.nav-menu').toggleClass('hide');
-// 		$('.month-button').toggleClass('active');
-// 		$('#month-dropdown').slideToggle('slow');
-// 	});
-//
-// 	$('.login-button').on('click', () => {
-// 		$('.login-form').slideToggle();
-// 	});
-//
-//
-// 	const createLi = () => {
-//
-// 		let active = false;
-//
-// 		let li = $('<li/>');
-// 		let span = $('<span/>');
-// 		span.text($('#myInput').val());
-// 		li.append(span);
-//
-// 		li.append($('<span/>')
-// 					.text('x')
-// 					.addClass('close')
-// 					.click(() => {
-// 						li.remove();
-// 					}))
-// 					.append($('<span/>', {"class": "material-icons edit"})
-// 						.text('edit'))
-// 					.on('click', '.edit', (e) => {
-// 						if (!active) {
-// 							active = true;
-// 							input = $('<input/>')
-// 									.val(span.text())
-// 									.on('keyup', (e) => {
-// 										if (e.keyCode === 13) {
-// 											span.text(e.target.value);
-// 											input.hide();
-// 											span.show();
-// 											active = false;
-// 										}
-// 									});
-// 							span.hide();
-// 							li.append(input);
-// 						}
-// 					})
-// 					.appendTo($('#myList'))
-// 					.on('click', () => {
-// 						span.toggleClass('checked')
-// 					})
-// 		$('#myInput').val('')
-// 	}
-//
-// 	$('#addBtn').on('click', createLi);
-//
-// 	$('#myInput').on('keyup', (e) => {
-// 		if (e.keyCode === 13) {
-// 			createLi();
-// 		}
-// 	});
-//
-//
-// 	$('#filter').on('change', function() {
-// 		if (this.value === 'all') {
-// 			showAll();
-// 		} else if (this.value === 'checked') {
-// 			showChecked();
-// 		} else {
-// 			showUnchecked();
-// 		}
-// 	});
-//
-// 	const showAll = () => {
-// 		$('li').show();
-// 	};
-//
-// 	const showChecked = () => {
-// 		$('li').hide();
-// 		$('span.checked').parent().show();
-// 	};
-//
-// 	const showUnchecked = () => {
-// 		$('li').hide();
-// 		$('li span:first-child').not('.checked').parent().show();
-// 	};
-//
-// 	const edit = event => {
-// 		$(event.currentTarget).hide();
-// 		$(event.currentTarget).text();
-// 		console.log('test');
-// 	}
-//
-// })
-
-
-
-
-
-
-
-// Using of JavaScript
-
-// const addNewElement = () => {
-// 	let newToDo = document.getElementById('myInput').value;
-// 	let ul = document.getElementById('myList');
-// 	let li = document.createElement('li');
-//
-// 	li.appendChild(document.createTextNode(newToDo));
-//
-//
-// 	ul.appendChild(li);
-//
-// 	document.getElementById('myInput').value = '';
-//
-// 	let closeButton = document.createElement('span');
-// 	closeButton.appendChild(document.createTextNode('x'));
-// 	closeButton.classList.add('close');
-// 	li.appendChild(closeButton);
-//
-// 	closeButton.onclick = () => {
-// 		li.remove();
-// 	};
-// };
-// const addCheckMark = () => {
-// 	let list = document.querySelector('ul');
-// 	list.addEventListener('click', function(ev) {
-// 	  if (ev.target.tagName === 'LI') {
-// 	    ev.target.classList.toggle('checked');
-// 	  }
-// 	}, false);
-// }
-// addCheckMark();
